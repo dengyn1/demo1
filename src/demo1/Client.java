33b1aab18dfe118c620e826413abe649d078edd2
dengyn <17693155728@163.com>
2019-08-29 11:02:50 +0800

    '输入和输出练习'

---
+++ src/demo1/Client.java
@@ -0,0 +1,14 @@
+package demo1;
+
+public class Client {
+
+	public static void main(String[] args) {
+		//第01步：创建Scanner对象
+		
+		//第02步：通过 name 变量接收用户的输入的姓名
+		
+		//第03步：输出欢迎信息，比如：“你好，张三”
+		
+	}
+
+}
